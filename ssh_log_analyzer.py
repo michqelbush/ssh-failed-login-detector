c0f654f5b1e34907b556671126f49f46b2792208
michqelbush <michqelbush@gmail.com>
2026-07-20 16:05:02 -0400

    Add auto IP blocking w/ iptables

--- ssh_log_analyzer.py
+++ ssh_log_analyzer.py
@@ -1,8 +1,12 @@
 import re
 import argparse
+import subprocess
 from collections import Counter
 
-def parse_auth_log(log_path, threshold):
+def block_ip(ip):
+	subprocess.run(["sudo", "iptables", "-A", "INPUT", "-s", ip, "-j", "DROP"], check=True)
+
+def parse_auth_log(log_path, threshold, block):
 	pattern = r"^(\S+) \S+ sshd-session\[\d+\]: Failed password for (\S+) from (\S+) port \d+"
 	failures_by_ip = Counter()
 
@@ -22,11 +26,15 @@ def parse_auth_log(log_path, threshold):
 	for ip, count in failures_by_ip.most_common():
 		flag = "SUS" if count >= threshold else ""
 		print(f"{ip}: {count} failed attempts {flag}")
+		if count >= threshold and block:
+			print(f"{ip} over threshold, blocked via iptables")
+			block_ip(ip)
 
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser(description="Parse SSH auth logs for failed attempst.")
 	parser.add_argument("logfile", nargs="?", default="/var/log/auth.log", help="Path to auth log file")
 	parser.add_argument("--threshold", type=int, default=3, help="Failed attempts before flagging as SUS")
+	parser.add_argument("--block", action="store_true", help="Actually block flagged IPs using iptables")
 	args = parser.parse_args()
 
-	parse_auth_log(args.logfile, args.threshold)
+	parse_auth_log(args.logfile, args.threshold, args.block)
